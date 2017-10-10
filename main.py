dde2fa4d19915ac7a6a3ab364c02e5a3758eb48c
Lucas <dlucasgf@gmail.com>
2017-10-10 14:46:08 -0300

    Correção do nome de alguns arquivos

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 # lista de documentos
 docs = [
 'A-Christmas-Carol-in-Prose--Being-a-Ghost-Story-of-Christmas-by-Charles Dickens.txt',
-'A-Doll-s-House--a-play-by-Henrik Ibsen.txt',
+'A-Doll-s-House---a-play-by-Henrik Ibsen.txt',
 'Adventures of Huckleberry Finn by Mark Twain.txt',
 'Alice-s Adventures in Wonderland by Lewis Carroll.txt',
 'An Inquiry into the Nature and Causes of the Wealth of Nations by Adam Smith.txt',
@@ -23,14 +23,14 @@ docs = [
 'Don Quixote by Miguel de Cervantes Saavedra.txt',
 'Dracula by Bram Stoker.txt',
 'Emma by Jane Austen.txt',
-'Frankenstein; Or, The Modern Prometheus by Mary Wollstonecraft Shelley.txt',
+'Frankenstein- Or, The Modern Prometheus by Mary Wollstonecraft Shelley.txt',
 'Great Expectations by Charles Dickens.txt',
 'Gulliver-s Travels into Several Remote Nations of the World by Jonathan Swift.txt',
-'Jane Eyre- An Autobiography by Charlotte Brontë.txt',
-'Les Misérables by Victor Hugo.txt',
+'Jane Eyre- An Autobiography by Charlotte Bronte.txt',
+'Les Miserables by Victor Hugo.txt',
 'Leviathan by Thomas Hobbes.txt',
 'Metamorphosis by Franz Kafka.txt',
-'Moby Dick; Or, The Whale by Herman Melville.txt',
+'Moby Dick Or, The Whale by Herman Melville.txt',
 'Oliver Twist by Charles Dickens.txt',
 'Paradise Lost by John Milton.txt',
 'Peter Pan by J. M. Barrie.txt',
@@ -53,7 +53,7 @@ docs = [
 'The Life and Adventures of Robinson Crusoe by Daniel Defoe.txt',
 'The Mysterious Affair at Styles by Agatha Christie.txt',
 'The Picture of Dorian Gray by Oscar Wilde.txt',
-'The Prince by Niccolò Machiavelli.txt',
+'The Prince by Niccolo Machiavelli.txt',
 'The Republic by Plato.txt',
 'The Return of Sherlock Holmes by Arthur Conan Doyle.txt',
 'The Scarlet Letter by Nathaniel Hawthorne.txt',
@@ -64,8 +64,8 @@ docs = [
 'The Tragedy of Romeo and Juliet by William Shakespeare.txt',
 'The War of the Worlds by H. G. Wells.txt',
 'The Wonderful Wizard of Oz by L. Frank Baum.txt',
-'The Works of Edgar Allan Poe — Volume 1 by Edgar Allan Poe.txt',
-'The Works of Edgar Allan Poe — Volume 2 by Edgar Allan Poe.txt',
+'The Works of Edgar Allan Poe - Volume 1 by Edgar Allan Poe.txt',
+'The Works of Edgar Allan Poe - Volume 2 by Edgar Allan Poe.txt',
 'Three Men in a Boat by Jerome K. Jerome.txt',
 'Through the Looking-Glass by Lewis Carroll.txt',
 'Treasure Island by Robert Louis Stevenson.txt',
@@ -73,7 +73,7 @@ docs = [
 'Utopia by Saint Thomas More.txt',
 'Walden, and On The Duty Of Civil Disobedience by Henry David Thoreau.txt',
 'War and Peace by graf Leo Tolstoy.txt',
-'Wuthering Heights by Emily Brontë.txt']
+'Wuthering Heights by Emily Bronte.txt']
 
 
 
