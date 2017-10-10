d8aad721dd3976f6d89c3ab09756786c4e03d1d1
Lucas <dlucasgf@gmail.com>
2017-10-10 14:24:59 -0300

    Correção do path

--- main.py
+++ main.py
@@ -89,6 +89,6 @@ class MySentences(object):
         for line in open(os.path.join(self.dirname, self.name)):
             yield line.split()
  
-sentences = MySentences('~/Documents/git/nlptp1/docs', 'Candide by Voltaire.txt') # a memory-friendly iterator
+sentences = MySentences('/home/lucas/Documents/git/nlptp1/docs', 'Candide by Voltaire.txt') # a memory-friendly iterator
 model = gensim.models.Word2Vec(sentences, iter=15)
 model.wv.save_word2vec_format('~/Documents/git/nlptp1/outs/voltaire', binary=False)
